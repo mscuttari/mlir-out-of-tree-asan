0eae2d491d2e452164a2cc6ac596facdf94b35c0
Michele Scuttari <michele.scuttari@outlook.com>
2025-09-18 22:40:10 +0200

    Improve run script

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd .docker
-docker build -t test-mlir-asan -f ./Dockerfile .
-cd ..
+docker build -t test-mlir-asan -f Dockerfile .docker
 docker run --rm -v "$(pwd)":/workspace -w /workspace test-mlir-asan bash -c "mkdir -p build && cmake -G Ninja -B build -S . && cmake --build build && ./build/bin/my-opt test/test.mlir"
